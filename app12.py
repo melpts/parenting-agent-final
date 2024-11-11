c60acfb41163cb077ca426cb681ba9b59e70a6bf
melpts <80393725+melpts@users.noreply.github.com>
2024-11-11 07:03:47 +0000

    Update app12.py

--- app12.py
+++ app12.py
@@ -379,7 +379,7 @@ def simulate_conversation_streamlit(name, child_age, situation):
         try:
             st.session_state['run_id'] = smith_client.create_run(
                 name="parenting_conversation",
-                run_type="conversation",  
+                run_type="chain",   
                 inputs={
                     "parent_name": name,
                     "child_name": child_name,
@@ -696,7 +696,7 @@ def display_advice(parent_name, child_age, situation):
                 # Log advice request to LangChain
                 run_id = smith_client.create_run(
                     name="parenting_advice",
-                    run_type="advice",  
+                    run_type="chain",  
                     inputs={
                         "parent_name": parent_name,
                         "child_age": child_age,
@@ -725,7 +725,7 @@ def display_conversation_starters(situation):
             # Log conversation starters to LangChain
             run_id = smith_client.create_run(
                 name="conversation_starters",
-                run_type="starters",  
+                run_type="chain", 
                 inputs={"situation": situation}
             ).id
             
@@ -756,7 +756,7 @@ def display_communication_techniques(situation):
                 # Log communication techniques to LangChain
                 run_id = smith_client.create_run(
                     name="communication_techniques",
-                    run_type="techniques",  
+                    run_type="chain", 
                     inputs={"situation": situation}
                 ).id
                 
