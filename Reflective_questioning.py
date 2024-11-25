e048a7b1728954be2adad45f0f6b1338b5a08793
melpts <mimobel24@gmail.com>
2024-11-25 00:26:14 +0000

    Update: Sync all supporting files with latest version

--- Reflective_questioning.py
+++ Reflective_questioning.py
@@ -5,4 +5,12 @@ Just in Time Parenting. (n.d.). Months 49-50. Retrieved from https://jitp.info/n
 Just in Time Parenting. (n.d.). Months 51-52. Retrieved from https://jitp.info/newsletter/months-51-52/
 HealthyChildren.org. (n.d.). Help Your Child Manage Fears and Anxieties. Retrieved from https://www.healthychildren.org/English/family-life/family-dynamics/Pages/help-your-child-manage-fears-and-anxieties.aspx
 HealthyChildren.org. (n.d.). Sibling Synergy. Retrieved from https://www.healthychildren.org/English/family-life/family-dynamics/Pages/Sibling-Synergy.aspx
+Center on the Developing Child at Harvard University. (n.d.). Activities Guide: Enhancing and Practicing Executive Function Skills with Children from Infancy to Adolescence. Retrieved from https://developingchild.harvard.edu/resources/activities-guide-enhancing-and-practicing-executive-function-skills-with-children/
+The Gottman Institute. (n.d.). Emotion Coaching: The Heart of Parenting. Retrieved from https://www.gottman.com/parents/
+American Psychological Association. (n.d.). Talking to Children About Stress. Retrieved from https://www.apa.org/topics/parenting/talking-to-children
+Mindset Works. (n.d.). Growth Mindset Parenting. Retrieved from https://www.mindsetworks.com/parents/growth-mindset-parenting
+Journal of Child and Family Studies. (2018). Journaling as a Tool for Emotional Regulation in Children. Retrieved from https://doi.org/10.1007/s10826-018-1147-8
+Lives in the Balance. (n.d.). What is Collaborative & Proactive Solutions (CPS)? Retrieved from https://www.livesinthebalance.org/what-is-cps/
+Triple P – Positive Parenting Program. (n.d.). What is Triple P? Retrieved from https://www.triplep-parenting.com/global/triple-p/
+Family Therapy Basics. (n.d.). Family Meetings: A Tool for Connection and Problem-Solving. Retrieved from https://familytherapybasics.com/articles/family-meetings
 """
